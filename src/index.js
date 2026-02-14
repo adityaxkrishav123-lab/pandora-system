b621af169d5ad5195f29b846f49875ca2d14543c
adityaxkrishav123-lab <adityaxkrishav123@gmail.com>
2026-02-15 02:18:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // This ensures Tailwind styles actually load!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
